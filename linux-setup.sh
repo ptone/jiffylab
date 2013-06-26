ce265f0a38685ae050c327580eb5cb3aeb3c9644
Preston Holmes <preston@ptone.com>
2013-06-25 23:20:47 -0700

    add some touches to provisioning

--- linux-setup.sh
+++ linux-setup.sh
@@ -5,6 +5,7 @@
 # manually if you are not using vagrant
 
 sudo apt-get install -q -y curl
+# if you are paying attention, no get.docker.io does NOT use https
 curl get.docker.io | sudo sh -x
 sudo apt-get install -y -q python-all-dev
 sudo apt-get install -y -q python-pip
@@ -18,11 +19,30 @@ if [ $WHO == 'vagrant' -o -d /vagrant/ ]
         cp -R /vagrant/* /usr/local/etc/jiffylab/
     else
         cp -r ./* /usr/local/etc/jiffylab/
-    fi
+fi
 
 pip install -r /usr/local/etc/jiffylab/webapp/requirements.txt
-# docker pull ptone/jiffy-base
-docker build /usr/local/etc/jiffylab/docker-builds/base
-echo "exec python /usr/local/etc/jiffylab/webapp/server.py" > /etc/init/jiffylabweb.conf
+
+stop dockerd
+echo "
+start on startup
+exec /usr/local/bin/docker -d" > /etc/init/dockerd.conf
+start dockerd
+# add a modicum of security - don't run the webapp as root
+useradd jiffylabweb
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
+chown -R jiffylabweb /usr/local/etc/jiffylab/webapp/
+
+docker pull ptone/jiffy-base
+# docker build /usr/local/etc/jiffylab/docker-builds/base
+
+echo "
+start on startup
+pre-start script
+   iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
+end script
+exec sudo -u jiffylabweb /usr/bin/python /usr/local/etc/jiffylab/webapp/server.py
+" > /etc/init/jiffylabweb.conf
+
 start jiffylabweb
 
--- rackspace-vagrant-env.sh
+++ rackspace-vagrant-env.sh
@@ -3,10 +3,10 @@
 # the values, then in any terminal where you want to do `vagrant up`, you can do
 #
 #   source <this file>
-#   vagrant up
+#   vagrant up (from inside the JiffyLab folder)
 
 export RS_USERNAME=""
 export RS_API_KEY=""
-export RS_PUBLIC_KEY="~/.ssh/id_rsa.pub" 
-export RS_PRIVATE_KEY="~/.ssh/id_rsa" 
+export RS_PUBLIC_KEY="~/.ssh/id_rsa.pub"
+export RS_PRIVATE_KEY="~/.ssh/id_rsa"
 export VAGRANT_DEFAULT_PROVIDER="rackspace"
