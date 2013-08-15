304de015eeb3257f6f29ef889205e73e66f92d67
Preston Holmes <preston@ptone.com>
2013-08-15 16:48:57 -0700

    rename and add other credentials to env holder

--- cloud-credentials.sh
+++ cloud-credentials.sh
@@ -3,10 +3,22 @@
 # the values, then in any terminal where you want to do `vagrant up`, you can do
 #
 #   source <this file>
-#   vagrant up (from inside the JiffyLab folder)
+#   vagrant up provider=<provider name>(from inside the JiffyLab folder)
 
+# RackSpace
 export RS_USERNAME=""
 export RS_API_KEY=""
 export RS_PUBLIC_KEY="~/.ssh/id_rsa.pub"
 export RS_PRIVATE_KEY="~/.ssh/id_rsa"
-export VAGRANT_DEFAULT_PROVIDER="rackspace"
+
+# DigitalOcean
+export DO_CLIENT_ID=""
+export DO_API_KEY=""
+
+# AWS
+export AWS_ACCESS_KEY=""
+export AWS_SECRET_KEY=""
+export AWS_KEYPAIR_NAME=""
+
+# optionally set your default provider
+# export VAGRANT_DEFAULT_PROVIDER=""
