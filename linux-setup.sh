febd08c40b946a69a17b98297c547a06fe00cf40
Preston Holmes <preston@ptone.com>
2013-08-15 23:13:14 -0700

    set webapp user to be in docker group
    this allows the webapp to use the socket file

--- linux-setup.sh
+++ linux-setup.sh
@@ -7,7 +7,6 @@
 # note that all of the sudo usage in this script is due to the uncertainty
 # of the provisioning user by vagrant.
 
-# sudo -s << EOF
 sudo apt-get install -q -y curl
 # if you are paying attention, no get.docker.io does NOT use https
 curl get.docker.io | sudo sh -x
@@ -31,16 +30,19 @@ sudo pip install -r /usr/local/etc/jiffylab/webapp/requirements.txt
 sudo stop dockerd
 sudo echo "
 start on startup
-exec /usr/local/bin/docker -d -H 127.0.0.1:4243" > /etc/init/dockerd.conf
+exec /usr/local/bin/docker -d" > /etc/init/dockerd.conf
 sudo start dockerd
-# add a modicum of security - don't run the webapp as root
-sudo useradd jiffylabweb
+# users the docker group can access the docker command and remote API
+sudo groupadd docker
+sudo useradd -g docker jiffylabweb
+
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
 sudo chown -R jiffylabweb /usr/local/etc/jiffylab/webapp/
 
 docker pull ptone/jiffylab-base
 # docker build /usr/local/etc/jiffylab/docker-builds/base
 
+# add a modicum of security - don't run the webapp as root
 sudo echo "
 start on startup
 pre-start script
@@ -50,4 +52,3 @@ exec sudo -u jiffylabweb /usr/bin/python /usr/local/etc/jiffylab/webapp/server.p
 " > /etc/init/jiffylabweb.conf
 
 sudo start jiffylabweb
-# EOF
